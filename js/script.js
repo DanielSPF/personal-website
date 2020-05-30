f8ab9f7bcea8c0e978a084e8914af6c5ebe9af3d
danielspf <dporto140@gmail.com>
2020-05-30 19:05:32 -0300

    Add ityped.js

---
+++ js/script.js
@@ -0,0 +1,5 @@
+/* iTyped */
+window.ityped.init(document.querySelector('.iTyped'), {
+    strings: ["I'm a Web Developer", 'I Love Html5', 'I Love CSS3', 'I Love Jquery', 'I Love Laravel'],
+    loop: true
+});
