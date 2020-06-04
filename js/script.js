f74bc88f4804855d8e84cfdaf6b2441c40b7e5da
danielspf <dporto140@gmail.com>
2020-06-04 18:51:55 -0300

    Avanços

--- js/script.js
+++ js/script.js
@@ -1,5 +1,20 @@
-/* iTyped */
+// iTyped 
+
 window.ityped.init(document.querySelector('.iTyped'), {
     strings: ["I'm a Web Developer", 'I Love Html5', 'I Love CSS3', 'I Love Jquery', 'I Love Laravel'],
     loop: true
 });
+
+// Portfolio Item Filter
+
+const filterContainer = document.querySelector('.portfolio-filter'),
+    filterBtns = filterContainer.children,
+    totalFilterBtn = filterBtns.length,
+    portfolioItems = document.querySelector('.portfolio-items').children;
+    console.log(portfolioItems);
+    for (let i = 0; i < totalFilterBtn; i++) {
+        filterBtns[i].addEventListener("click", function(){
+            filterContainer.querySelector('.active').classList.remove('active');
+            this.classList.add("active");
+        });
+    }
