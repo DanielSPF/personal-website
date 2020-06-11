47373c13ca7ca84dd13c39280ac1d11f521b5105
danielspf <dporto140@gmail.com>
2020-06-11 19:12:04 -0300

    Avanços

--- js/script.js
+++ js/script.js
@@ -83,8 +83,50 @@ function nextItem() {
 }
 
 // close lightbox
+
 lightbox.addEventListener('click', function(event){
     if(event.target === lightboxClose || event.target === lightbox){
         toggleLightbox();
     }
 });
+
+// Aside Navbar
+
+const nav = document.querySelector('.nav'),
+    navList = nav.querySelectorAll('li'),
+    totalNavList = navList.length,
+    allSection = document.querySelectorAll('.section'),
+    totalSection = allSection.length;
+
+for (let i = 0; i < totalNavList; i++) {
+    const a = navList[i].querySelector('a');
+    a.addEventListener('click', function(){
+        // remove back section class
+        for (let i = 0; i < totalSection; i++) {
+            allSection[i].classList.remove('back-section');
+        }
+
+        for (let j = 0; j < totalNavList; j++) {
+            if (navList[j].querySelector('a').classList.contains('active')) {
+                // add back section class
+                allSection[j].classList.add('back-section');
+            }
+            navList[j].querySelector('a').classList.remove('active');
+        }
+
+        this.classList.add('active');
+
+        showSection(this);
+    });
+}
+
+function showSection(element) 
+{
+    for (let i = 0; i < totalSection; i++) {
+        allSection[i].classList.remove('active');
+    }
+
+    const target = element.getAttribute('href').split('#')[1];
+
+    document.querySelector('#'+target).classList.add('active');
+}
